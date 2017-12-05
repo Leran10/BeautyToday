b2f805848c8ff959993e3d2618f4b353b5bab13b
leran <leranwang10@gmail.com>
2017-12-05 12:34:08 +0800

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/beautytoday20/MainActivity.java
@@ -0,0 +1,86 @@
+package com.bignerdranch.android.beautytoday20;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mCoatButton;
+    Button mDressButton;
+    Button mTrousersButton;
+    Button mSkirtButton;
+    Button mBootsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCoatButton = (Button)findViewById(R.id.coat);
+        mCoatButton.setOnClickListener(new View.OnClickListener() {
+            FragmentManager fragmentmanager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+            @Override
+            public void onClick(View v) {
+                coatFragment coat1 = new coatFragment();
+                fragmentTransaction.add(R.id.fragment_container,coat1);
+                fragmentTransaction.commit();
+            }
+        });
+
+        mDressButton = (Button)findViewById(R.id.dress);
+        mDressButton.setOnClickListener(new View.OnClickListener() {
+            FragmentManager fragmentmanager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+            @Override
+            public void onClick(View v) {
+                dressFragment dress1 = new dressFragment();
+                fragmentTransaction.add(R.id.fragment_container,dress1);
+                fragmentTransaction.commit();
+            }
+        });
+
+        mTrousersButton = (Button)findViewById(R.id.trousers);
+        mTrousersButton.setOnClickListener(new View.OnClickListener() {
+            FragmentManager fragmentmanager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+            @Override
+            public void onClick(View v) {
+                TrousersFragment trousers1 = new TrousersFragment();
+                fragmentTransaction.add(R.id.fragment_container,trousers1);
+                fragmentTransaction.commit();
+            }
+        });
+
+        mSkirtButton = (Button)findViewById(R.id.skirt);
+        mSkirtButton.setOnClickListener(new View.OnClickListener() {
+            FragmentManager fragmentmanager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+            @Override
+            public void onClick(View v) {
+                skirtFragment skirt1 = new skirtFragment();
+                fragmentTransaction.add(R.id.fragment_container,skirt1);
+                fragmentTransaction.commit();
+            }
+        });
+
+        mBootsButton = (Button)findViewById(R.id.boots);
+        mBootsButton.setOnClickListener(new View.OnClickListener() {
+            FragmentManager fragmentmanager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+            @Override
+            public void onClick(View v) {
+                bootsFragment boots1 = new bootsFragment();
+                fragmentTransaction.add(R.id.fragment_container,boots1);
+                fragmentTransaction.commit();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/beautytoday20/bootsFragment.java
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.beautytoday20;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class bootsFragment extends Fragment {
+
+
+    public bootsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.boots_fragment_layout, container, false);
+    }
+
+}
