65ac59d34c4fad908e229cbe80df0a48c602193f
leran <leranwang10@gmail.com>
2017-12-08 14:59:10 +0800

    got taking picture work

--- app/src/main/java/com/bignerdranch/android/beautytoday20/coatFragment.java
+++ app/src/main/java/com/bignerdranch/android/beautytoday20/coatFragment.java
@@ -3,8 +3,10 @@ package com.bignerdranch.android.beautytoday20;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +27,7 @@ import model.ItemData;
 import model.coat;
 
 import static android.R.attr.duration;
+import static android.app.Activity.RESULT_OK;
 import static java.sql.DriverManager.println;
 
 
@@ -33,13 +37,15 @@ import static java.sql.DriverManager.println;
 public class coatFragment extends Fragment {
 
     private Context context;
-    private Button mButton;
-    private EditText mCoatColor;
-    private EditText mCoatPattern;
-    private EditText mCoatMaterial;
-    private EditText mCoatStyle;
-    private EditText mCoatSleeves;
-    private EditText mCoatLength;
+    private Button mButtonCon;
+    private Button mButtonCam;
+    private ImageView result;
+    private Spinner mCoatColor;
+    private Spinner mCoatPattern;
+    private Spinner mCoatMaterial;
+    private Spinner mCoatStyle;
+    private Spinner mCoatSleeves;
+    private Spinner mCoatLength;
     private DatabaseHelper databasehelper;
     private coat c;
     private String color;
@@ -48,6 +54,8 @@ public class coatFragment extends Fragment {
     private String style;
     private String sleeves;
     private String length;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     public coatFragment() {
@@ -92,72 +100,77 @@ public class coatFragment extends Fragment {
         SpinnerAdapter adapter1 = new SpinnerAdapter(getActivity(),R.layout.spinner_layout, R.id.txt, list1);
         sp1.setAdapter(adapter1);
 
-        ArrayList<ItemData> list2 = new ArrayList<>();
-        list1.add(new ItemData("no pattern",R.drawable.white));
-        list1.add(new ItemData("small floral",R.drawable.smallfloral));
-        list1.add(new ItemData("big floral",R.drawable.bigfloral));
-        list1.add(new ItemData("circle",R.drawable.circle));
-        list1.add(new ItemData("checked",R.drawable.checked));
-        list1.add(new ItemData("carton",R.drawable.carton));
-        list1.add(new ItemData("abstract",R.drawable.abstract1));
-        list1.add(new ItemData("paint",R.drawable.paint));
-        Spinner sp2 = (Spinner)rootview.findViewById(R.id.patternSpinner);
+       ArrayList<ItemData> list2 = new ArrayList<>();
+        list2.add(new ItemData("leather",R.drawable.leather));
+        list2.add(new ItemData("cotton",R.drawable.cotton));
+        list2.add(new ItemData("chiffon",R.drawable.chiffon));
+        list2.add(new ItemData("flax",R.drawable.flax));
+        list2.add(new ItemData("down or cotton",R.drawable.downorcotton));
+        list2.add(new ItemData("silk",R.drawable.silk));
+        list2.add(new ItemData("waterproof fabric",R.drawable.waterprooffabric));
+        list2.add(new ItemData("wool",R.drawable.wool));
+        list2.add(new ItemData("denim",R.drawable.denim));
+        Spinner sp2 = (Spinner)rootview.findViewById(R.id.materialSpinner);
         SpinnerAdapter adapter2 = new SpinnerAdapter(getActivity(),R.layout.spinner_layout, R.id.txt, list2);
         sp2.setAdapter(adapter2);
 
         ArrayList<ItemData> list3 = new ArrayList<>();
-        list1.add(new ItemData("no pattern",R.drawable.white));
-        list1.add(new ItemData("small floral",R.drawable.smallfloral));
-        list1.add(new ItemData("big floral",R.drawable.bigfloral));
-        list1.add(new ItemData("circle",R.drawable.circle));
-        list1.add(new ItemData("checked",R.drawable.checked));
-        list1.add(new ItemData("carton",R.drawable.carton));
-        list1.add(new ItemData("abstract",R.drawable.abstract1));
-        list1.add(new ItemData("paint",R.drawable.paint));
-        Spinner sp3 = (Spinner)rootview.findViewById(R.id.materialSpinner);
+        list3.add(new ItemData("suit",R.drawable.white));
+        list3.add(new ItemData("bussiness casual",R.drawable.white));
+        list3.add(new ItemData("outdoor",R.drawable.white));
+        list3.add(new ItemData("ladies",R.drawable.white));
+        list3.add(new ItemData("punk",R.drawable.white));
+        list3.add(new ItemData("evening dress",R.drawable.white));
+        list3.add(new ItemData("saxy",R.drawable.white));
+        list3.add(new ItemData("mori girl",R.drawable.white));
+        list3.add(new ItemData("natual style",R.drawable.white));
+        Spinner sp3 = (Spinner)rootview.findViewById(R.id.styleSpinner);
         SpinnerAdapter adapter3 = new SpinnerAdapter(getActivity(),R.layout.spinner_layout, R.id.txt, list3);
         sp3.setAdapter(adapter3);
 
-        ArrayList<ItemData> list4 = new ArrayList<>();
-        list1.add(new ItemData("long sleeves",R.drawable.longsleeves));
-        list1.add(new ItemData("short sleeves",R.drawable.shortsleeves));
+         ArrayList<ItemData> list4 = new ArrayList<>();
+        list4.add(new ItemData("long sleeves",R.drawable.longsleeves));
+        list4.add(new ItemData("short sleeves",R.drawable.shortsleeves));
+        list4.add(new ItemData("vest",R.drawable.vest));
+        list4.add(new ItemData("sling",R.drawable.sling));
         Spinner sp4 = (Spinner)rootview.findViewById(R.id.sleevesSpinner);
         SpinnerAdapter adapter4 = new SpinnerAdapter(getActivity(),R.layout.spinner_layout, R.id.txt, list4);
         sp4.setAdapter(adapter4);
 
         ArrayList<ItemData> list5 = new ArrayList<>();
-        list1.add(new ItemData("no pattern",R.drawable.white));
-        list1.add(new ItemData("small floral",R.drawable.smallfloral));
-        list1.add(new ItemData("big floral",R.drawable.bigfloral));
-        list1.add(new ItemData("circle",R.drawable.circle));
-        list1.add(new ItemData("checked",R.drawable.checked));
-        list1.add(new ItemData("carton",R.drawable.carton));
-        list1.add(new ItemData("abstract",R.drawable.abstract1));
-        list1.add(new ItemData("paint",R.drawable.paint));
-        Spinner sp5 = (Spinner)rootview.findViewById(R.id.sleevesSpinner);
+        list5.add(new ItemData("long",R.drawable.longcoat));
+        list5.add(new ItemData("short",R.drawable.shortcoat));
+        Spinner sp5 = (Spinner)rootview.findViewById(R.id.lengthSpinner);
         SpinnerAdapter adapter5 = new SpinnerAdapter(getActivity(),R.layout.spinner_layout, R.id.txt, list5);
         sp5.setAdapter(adapter5);
 
-        mButton = (Button)rootview.findViewById(R.id.confirm);
-        mCoatColor = (EditText)rootview.findViewById(R.id.colorField);
-        mCoatPattern = (EditText)rootview.findViewById(R.id.patternField);
-        mCoatMaterial = (EditText)rootview.findViewById(R.id.materialField);
-        mCoatStyle = (EditText)rootview.findViewById(R.id.styleField);
-        mCoatSleeves = (EditText)rootview.findViewById(R.id.coatSleeves);
-        mCoatLength = (EditText)rootview.findViewById(R.id.coatlength);
+        mButtonCon = (Button)rootview.findViewById(R.id.confirm);
+        mButtonCam = (Button)rootview.findViewById(R.id.camera);
+        result = (ImageView)rootview.findViewById(R.id.img);
+        mCoatColor = (Spinner)rootview.findViewById(R.id.colorSpinner);
+        mCoatPattern = (Spinner)rootview.findViewById(R.id.patternSpinner);
+        mCoatMaterial = (Spinner)rootview.findViewById(R.id.materialSpinner);
+        mCoatStyle = (Spinner)rootview.findViewById(R.id.styleSpinner);
+        mCoatSleeves = (Spinner)rootview.findViewById(R.id.sleevesSpinner);
+        mCoatLength = (Spinner)rootview.findViewById(R.id.lengthSpinner);
+
+        mButtonCam.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
 
-
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mButtonCon.setOnClickListener(new View.OnClickListener() {
 
                 public void onClick(View v) {
                     //Log.d("test","string1");
-                    color = mCoatColor.getText().toString();
-                    pattern = mCoatPattern.getText().toString();
-                    material = mCoatMaterial.getText().toString();
-                    style = mCoatStyle.getText().toString();
-                    sleeves = mCoatSleeves.getText().toString();
-                    length = mCoatLength.getText().toString();
+                    color = mCoatColor.getSelectedItem().toString();
+                    pattern = mCoatPattern.getSelectedItem().toString();
+                    material = mCoatMaterial.getSelectedItem().toString();
+                    style = mCoatStyle.getSelectedItem().toString();
+                    sleeves = mCoatSleeves.getSelectedItem().toString();
+                    length = mCoatLength.getSelectedItem().toString();
 
                     databasehelper = new DatabaseHelper(getActivity());
                     c = new coat();
@@ -201,6 +214,22 @@ public class coatFragment extends Fragment {
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            result.setImageBitmap(imageBitmap);
+        }
     }
 
 }
